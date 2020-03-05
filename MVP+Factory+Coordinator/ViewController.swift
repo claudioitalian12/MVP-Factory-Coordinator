6ea46f2501d6d5881698af761f65908d609f1037
Claudio Cavalli <dark_claudio@hotmail.it>
2020-03-05 11:59:12 +0100

    Initial Commit

---
+++ MVP+Factory+Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP+Factory+Coordinator
+//
+//  Created by claudio cavalli on 05/03/2020.
+//  Copyright © 2020 claudio cavalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
